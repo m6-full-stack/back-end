2dfdc185638ffd69dc3b177899dede6af48a4f55
Ricardo Martins <102545943+ORMartins02@users.noreply.github.com>
2023-03-01 14:20:04 -0300

    fix: migration correction

--- src/migrations/1677691130330-initialMigration.ts
+++ src/migrations/1677691130330-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1677688967056 implements MigrationInterface {
+export class initialMigration1677691130330 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
