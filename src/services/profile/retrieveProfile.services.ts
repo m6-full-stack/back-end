cb22acd428723c5002eacbe4708e8b53da69a837
Ricardo Martins <102545943+ORMartins02@users.noreply.github.com>
2023-03-01 21:14:47 -0300

    feat: profile data implementation

--- src/services/profile/retrieveProfile.services.ts
+++ src/services/profile/retrieveProfile.services.ts
@@ -5,7 +5,7 @@ import { AppError } from "../../errors/appError";
 export const retrieveProfileService = async (id: string): Promise<User> => {
   const userRespository = AppDataSource.getRepository(User);
 
-  const user = await userRespository.findOneBy({id: id});
+  const user = await userRespository.findOne({relations: {announcements: true}, where: {id: id}});
 
   if (!user) {
     throw new AppError(404, "User not Found");
