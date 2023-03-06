b049927f7a7120728d3b639bd174f50de198b2d1
Pedro Gabriel <pedrogv03@hotmail.com>
2023-03-06 10:46:43 -0300

    feat: add user return data all announcement

--- src/controllers/announcements/announcementList.controller.ts
+++ src/controllers/announcements/announcementList.controller.ts
@@ -1,11 +1,11 @@
+import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import announcementListService from "../../services/announcements/announcementList.services";
 
 const announcementListController = async (req: Request, res: Response) => {
-
   const announcement = await announcementListService();
 
-  return res.send(announcement);
+  return res.send(instanceToPlain(announcement));
 };
 
 export default announcementListController;
