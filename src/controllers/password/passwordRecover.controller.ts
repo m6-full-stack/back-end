58c964c803cc0198b2d7adbf02d85b65cd598e6a
Axel <axeellima@gmail.com>
2023-03-07 21:33:44 -0300

    fix: route control password recover

--- src/controllers/password/passwordRecover.controller.ts
+++ src/controllers/password/passwordRecover.controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express'
 import { instanceToPlain } from 'class-transformer'
 import passwordRecoverService from '../../services/password/passwordRecover.service'
 const passwordRecoverController = async (req: Request, res: Response) => {
-    const { tokenResetPassword, password } = req.body
+    const { id } = req.params
+    const tokenResetPassword = id
+    const { password } = req.body
     const updated = await passwordRecoverService(tokenResetPassword, password)
     return res.status(200).send(instanceToPlain(updated))
 }
